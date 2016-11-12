fdb6c8ef52ab98ab75a89bd405a0d9c5df22d0a4
Manfred Stock <mstock@cpan.org>
2016-11-12 16:39:11 +0100

    Fork in the basic_log_test
    Seems to make it work with newer versions of the used ZeroMQ libraries.
    Otherwise, the tests just hung as soon as $cv->recv() was executed.

--- t/lib/Plack/Middleware/AccessLog/Structured/ZeroMQTest.pm
+++ t/lib/Plack/Middleware/AccessLog/Structured/ZeroMQTest.pm
@@ -20,44 +20,54 @@ use Message::Passing::Input::ZeroMQ;
 sub basic_log_test : Test(2) {
 	my ($self) = @_;
 
-	my $app = sub { [200, ['Content-Type' => 'text/plain'], ['ok']] };
 	my $connect = 'tcp://127.0.0.1:'.empty_port();
-	my $wrapped_app = Plack::Middleware::AccessLog::Structured::ZeroMQ->wrap($app,
-		connect => $connect,
-	);
+	my $pid = fork();
+	if ($pid) {
+		my $input = $self->create_input($connect);
 
-	test_psgi($wrapped_app, sub {
-		my ($cb) = @_;
-		my $response = $cb->(GET '/', Referer => 'http://localhost/foo');
-	});
+		is($input->output_to()->output_to()->message_count(), 1, 'message count ok');
+		cmp_deeply(
+			[$input->output_to()->output_to()->messages()],
+			[{
+				class            => 'Plack::Middleware::AccessLog::Structured::ZeroMQ',
+				hostfqdn         => re('^[\w\.-]+$'),
+				hostname         => re('^[\w\.-]+$'),
+				http_host        => 'localhost',
+				http_user_agent  => undef,
+				http_referer     => 'http://localhost/foo',
+				remote_user      => undef,
+				pid              => $pid,
+				remote_addr      => '127.0.0.1',
+				request_duration => re('^\d+\.\d+$'),
+				request_method   => 'GET',
+				request_uri      => '/',
+				response_status  => 200,
+				content_length   => 2,
+				content_type     => 'text/plain',
+				server_protocol  => 'HTTP/1.1',
+				date             => re('^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$'),
+				epochtime        => re('^\d+(?:\.\d+)?$')
+			}],
+			'log message ok'
+		);
+	}
+	elsif (defined $pid) {
+		my $app = sub { [200, ['Content-Type' => 'text/plain'], ['ok']] };
 
-	my $input = $self->create_input($connect);
+		my $wrapped_app = Plack::Middleware::AccessLog::Structured::ZeroMQ->wrap($app,
+			connect => $connect,
+		);
 
-	is($input->output_to()->output_to()->message_count(), 1, 'message count ok');
-	cmp_deeply(
-		[$input->output_to()->output_to()->messages()],
-		[{
-			class            => 'Plack::Middleware::AccessLog::Structured::ZeroMQ',
-			hostfqdn         => re('^[\w\.-]+$'),
-			hostname         => re('^[\w\.-]+$'),
-			http_host        => 'localhost',
-			http_user_agent  => undef,
-			http_referer     => 'http://localhost/foo',
-			remote_user      => undef,
-			pid              => $$,
-			remote_addr      => '127.0.0.1',
-			request_duration => re('^\d+\.\d+$'),
-			request_method   => 'GET',
-			request_uri      => '/',
-			response_status  => 200,
-			content_length   => 2,
-			content_type     => 'text/plain',
-			server_protocol  => 'HTTP/1.1',
-			date             => re('^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$'),
-			epochtime        => re('^\d+(?:\.\d+)?$')
-		}],
-		'log message ok'
-	);
+		test_psgi($wrapped_app, sub {
+			my ($cb) = @_;
+			my $response = $cb->(GET '/', Referer => 'http://localhost/foo');
+		});
+
+		exit;
+	}
+	else {
+		die 'Failed to fork';
+	}
 }
 
 
